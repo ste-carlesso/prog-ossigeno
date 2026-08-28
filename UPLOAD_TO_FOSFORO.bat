d0327e8621fba66176338ee32e4b730b77ebb05d
Stefano Carlesso <s.carlesso@fondazioneomd.it>
2026-08-28 12:22:47 +0100

    Add task scheduler  configuration
    	new file:   upload_to_fosforo.xml

--- UPLOAD_TO_FOSFORO.bat
+++ UPLOAD_TO_FOSFORO.bat
@@ -1,2 +1,2 @@
 "D:\script_ossigeno\env\Scripts\python.exe" "D:\script_ossigeno\upload_to_fosforo.py"
-REM "D:\script_ossigeno\env\Scripts\python.exe" "D:\script_ossigeno\upload_raw.py"
+"D:\script_ossigeno\env\Scripts\python.exe" "D:\script_ossigeno\upload_raw.py"
