bf79983295b495ac02731feab388d901069ed54e
Stefano Carlesso <s.carlesso@fondazioneomd.it>
2026-09-08 15:04:15 +0200

    Add scheduling to upload to Fosforo.

--- 90_the_scheduling_proper.bat
+++ 90_the_scheduling_proper.bat
@@ -19,6 +19,19 @@ SCHTASKS /CREATE ^
  /TN "invia_a_V1" ^
  /TR D:\script_ossigeno\21_upload_to_V1.bat ^
  /SC daily ^
- /ST 05:30 ^
+ /ST 06:00 ^
+ /RU OSSIGENO\Fondazione ^
+ /RP Porta100
+
+SCHTASKS /DELETE ^
+/TN "invia_a_fosforo" ^
+/F
+
+
+SCHTASKS /CREATE ^
+ /TN "invia_a_fosforo" ^
+ /TR D:\script_ossigeno\UPLOAD_TO_FOSFORO.bat ^
+ /SC daily ^
+ /ST 06:50 ^
  /RU OSSIGENO\Fondazione ^
  /RP Porta100
